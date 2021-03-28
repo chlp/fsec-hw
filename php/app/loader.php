d7c062e4944a111314f088e29d9c63b2144420ea
Aleksei Rytikov <user.raa@gmail.com>
2021-03-28 15:31:54 +0300

    add Supervisor.php to loader

--- php/app/loader.php
+++ php/app/loader.php
@@ -5,6 +5,7 @@ namespace App;
 require __DIR__ . '/Utility/Config.php';
 require __DIR__ . '/Utility/Logger.php';
 require __DIR__ . '/Utility/Validator.php';
+require __DIR__ . '/Utility/Supervisor.php';
 require __DIR__ . '/DataStream/DataStreamService.php';
 require __DIR__ . '/Queue/QueueService.php';
 require __DIR__ . '/Telemetry/Message/TelemetryMessage.php';
