6f2d4e4df63f5bae006833b334ee167fcc5ee1e2
Aleksei Rytikov <user.raa@gmail.com>
2021-03-28 12:50:30 +0300

    loader.php

--- php/app/loader.php
+++ php/app/loader.php
@@ -3,6 +3,7 @@
 namespace App;
 
 require __DIR__ . '/Utility/Logger.php';
+require __DIR__ . '/Utility/Validator.php';
 require __DIR__ . '/Telemetry/Message/TelemetryMessage.php';
 require __DIR__ . '/Telemetry/Event/TelemetryEvent.php';
 require __DIR__ . '/Telemetry/Event/NewProcessEvent.php';
